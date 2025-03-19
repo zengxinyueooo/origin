2974eb16da10d262a4b67fe8b4b8160a65e0b1b6
ll <2005>
2025-03-19 22:12:39 +0800

    实体类

--- navigation-pojo/src/main/java/com/navigation/entity/Region.java
+++ navigation-pojo/src/main/java/com/navigation/entity/Region.java
@@ -1,4 +1,4 @@
 package com.navigation.entity;
 
-public class User {
+public class Region {
 }
---
+++ navigation-pojo/src/main/java/com/navigation/entity/TicketReservation.java
@@ -0,0 +1,36 @@
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * 票务预定实体类
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketReservation {
+    private Integer reservationId; // 预定ID
+
+    private Integer userId; // 用户ID
+
+    private Integer ticketId; // 门票ID
+
+    private Integer quantity; // 预定的数量
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime reservationTime; // 预定的时间
+
+    private Integer status; // 预定状态，0表示未支付
+
+    private BigDecimal totalPrice; // 总金额
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime; // 创建时间
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateTime; // 修改时间
+}
