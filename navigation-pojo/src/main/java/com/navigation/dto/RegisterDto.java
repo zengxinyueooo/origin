0dbc1b2641c7a342dd612f23584e73ed6220b1d5
ll <2005>
2025-04-10 21:05:52 +0800

    邮件修改

--- navigation-pojo/src/main/java/com/navigation/dto/RegisterDto.java
+++ navigation-pojo/src/main/java/com/navigation/dto/RegisterDto.java
@@ -17,8 +17,7 @@ import java.time.LocalDateTime;
 @ApiModel("用户注册请求参数")
 public class RegisterDto {
 
-    @ApiModelProperty(value = "昵称", required = true)
-    @NotBlank(message = "昵称不能为空")
+    @ApiModelProperty(value = "昵称", required = false)
     private String nickName;
 
     @ApiModelProperty(value = "邮箱", required = true)
@@ -31,11 +30,11 @@ public class RegisterDto {
     @Size(min = 6, message = "密码长度不能小于6位")
     private String password;
 
-    @ApiModelProperty(value = "用户年龄", required = true)
+    @ApiModelProperty(value = "用户年龄", required = false)
     @NotNull(message = "年龄不能为空")
     private Integer age;
 
-    @ApiModelProperty(value = "性别 (M: 男, F: 女)", required = true)
+    @ApiModelProperty(value = "性别 (M: 男, F: 女)", required = false)
     @NotBlank(message = "性别不能为空")
     private String gender;
 
